9b9738c7f4c1e2a7dde277b6f24bd66dd0df57d5
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-20 18:17:47 +0900

    DataBinding 작업

--- app/src/main/java/kr/co/yhw/loginpage_20200520_test/MainActivity.java
+++ app/src/main/java/kr/co/yhw/loginpage_20200520_test/MainActivity.java
@@ -1,14 +1,22 @@
 package kr.co.yhw.loginpage_20200520_test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import kr.co.yhw.loginpage_20200520_test.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
     }
 }
